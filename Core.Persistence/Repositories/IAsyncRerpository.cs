b02649ad89cdf44f5bf991554f5a7a92db4774f4
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-09-27 23:31:57 +0300

    Preparation of EfRepository class. Processing of Soft Delete feature.

--- Core.Persistence/Repositories/IAsyncRerpository.cs
+++ Core.Persistence/Repositories/IAsyncRerpository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Core.Persistence.Repositories;
 
-public interface IAsyncRepository<TEntity,TEntityId> : IQueryable<TEntity>
+public interface IAsyncRepository<TEntity,TEntityId> : IQuery<TEntity>
     where TEntity : Entity<TEntityId>
 {
     Task<TEntity?> GetAsync(
