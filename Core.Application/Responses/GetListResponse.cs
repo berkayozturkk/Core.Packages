6b63a352a6be7005f73238ba8109f97b9a876672
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-10-02 19:51:49 +0300

    Introducing the GetListResponse class.

---
+++ Core.Application/Responses/GetListResponse.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Responses;
+
+public class GetListResponse<T> : BasePageableModel
+{
+    private IList<T> items;
+
+    public IList<T> Items 
+    { 
+        get => items ?? new List<T>();
+        set => items = value;
+    }
+}
