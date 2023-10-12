5544e633748bd21247503d1a10c82ef7f5d00dc8
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-10-12 20:13:31 +0300

    Middleware infrastructure for error management.

---
+++ Core.CrossCuttingConcerns/Exceptions/Extentions/ExceptionMiddlewareExtentions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extentions;
+
+public static class ExceptionMiddlewareExtentions
+{
+    public static void ConfigureCostumExceptionMiddleware
+        (this IApplicationBuilder app)
+        => app.UseMiddleware<ExceptionMiddleware>();
+}
