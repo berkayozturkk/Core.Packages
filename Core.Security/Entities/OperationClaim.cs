139aa884b965940f656e3bae68ce7fe5f7ac5ab3
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2024-01-20 21:24:46 +0300

    Creation of security layer assets

---
+++ Core.Security/Entities/OperationClaim.cs
@@ -0,0 +1,22 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Core.Security.Entities;
+
+public class OperationClaim : Entity<int>
+{
+    public string Name { get; set; }
+
+    public virtual ICollection<UserOperationClaim> UserOperationClaims { get; set; }
+
+    public OperationClaim() {}
+
+    public OperationClaim(string name)
+    {
+        Name = name;
+    }
+
+    public OperationClaim(int id,string name) : base(id) 
+    {
+        Name = name;
+    }
+}
